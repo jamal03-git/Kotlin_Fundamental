a5ec3fd6a607bde42d134cd71e55a3c6f3b40de5
jamal03-git <3103119122@student.smktelkom-pwt.sch.id>
2020-08-15 23:16:50 +0700

    bolean

---
+++ src/main/kotlin/Bolean.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val nilai = 85
+    if (nilai >= 90) {
+        print("A")
+    } else if (nilai >= 80 && nilai < 90) {
+        print("B")
+    } else if (nilai >= 70 && nilai < 80) {
+        print("C")
+    } else if (nilai >= 60 && nilai < 70) {
+        print("D")
+    } else {
+        print("E")
+    }
+}
